044b7683eb153e31c76299f8076494ec94fe5a07
Beau Labs <46016124+beaulabs@users.noreply.github.com>
2019-05-14 21:26:43 -0600

    adjusted account vars

--- main.tf
+++ main.tf
@@ -3,9 +3,8 @@
 # Define authentification configuration
 provider "vsphere" {
   # If you use a domain set your login like this "MyDomain\\MyUser"
-  user           = "${var.vsphere_user}"
-  password       = "${var.vsphere_password}"
-  vsphere_server = "${var.vsphere_server}"
+
+  # user           = "${var.vsphere_user}"    # password       = "${var.vsphere_password}"    # vsphere_server = "${var.vsphere_server}"
 
   # if you have a self-signed cert
   allow_unverified_ssl = true
